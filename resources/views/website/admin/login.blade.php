dc216da055b3d7aaa03ca697d8d0cc2592582954
Edrees1995 <edreessalih1@gmail.com>
2023-09-02 14:53:42 +0400

    Design login regsiter user and login admin

--- resources/views/website/admin/login.blade.php
+++ resources/views/website/admin/login.blade.php
@@ -6,47 +6,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Login</title>
+    <!-- Add Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
+    <!-- Add Font Awesome CSS for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('assets/css/admin/login.css') }}">
 </head>
 
 <body>
 
-    <div class="container">
-        <h2 style="margin-top: 30px;">Admin Login Form</h2>
-        <form action="{{ route('admin.create-login') }}" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                    id="email" placeholder="Enter email" value="{{ old('email') }}">
-                @error('email')
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @enderror
+    <div class="container mt-5">
+        <div class="card mt-5">
+            <div class="user-profile">
+                <img src="{{ asset('assets/global-img/user.png') }}" alt="User Profile" />
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                    id="password" placeholder="Enter password">
-                @error('password')
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <div class="user-title text-center">Admin Login</div>
+            <form action="{{ route('admin.create-login') }}" method="POST">
+                @csrf
+                <div class="mb-3"> <!-- Center-align the label -->
+                    <label for="email" class="form-label">Email address</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fas fa-envelope"></i></span>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" placeholder="Enter email" value="{{ old('email') }}">
                     </div>
-                @enderror
+                    @error('email')
+                        <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                            <i class="fas fa-exclamation-circle"></i> {{ $message }}
+                            <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-3"> <!-- Center-align the label -->
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fas fa-lock"></i></span>
+                        <input type="password" name="password"
+                            class="form-control @error('password') is-invalid @enderror" id="password"
+                            placeholder="Enter password">
+                    </div>
+                    @error('password')
+                        <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                            <i class="fas fa-exclamation-circle"></i> {{ $message }}
+                            <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-outline-success btn-block">Login</button>
+            </form>
+            <div class="mt-3 text-center">
+                <a href="{{ route('user.register') }}"> Forget password? </a>.
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
-        <div class="mt-3">
-            Don't have an account? <a href="{{ route('user.register') }}">Register here</a>.
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous">
-    </script>
+    <!-- Add Bootstrap and Popper.js scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 
--- resources/views/website/user/register.blade.php
+++ resources/views/website/user/register.blade.php
@@ -5,74 +5,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User Registeration</title>
+    <title>User Registration</title>
+    <!-- Add Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
+    <!-- Add Font Awesome CSS for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('assets/css/user/register.css') }}">
 </head>
 
 <body>
 
-    <div class="container">
-        <h2 style="margin-top: 30px;">User Registration Form</h2>
-        <form action="{{ route('user.create') }}" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="name" class="form-label">Fullname</label>
-                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
-                    id="name" placeholder="Enter full name" value="{{ old('name') }}">
-                @error('name')
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="register-container container mb-5">
+        <div class="card mt-5">
+            <div class="user-profile">
+                <img src="{{ asset('assets/global-img/user.png') }}" alt="User Profile" />
+            </div>
+            <div class="user-title text-center">User Registration</div>
+            <form action="{{ route('user.create') }}" method="POST">
+                @csrf
+                <div> <!-- Center-align the label -->
+                    <label for="name" class="form-label">Fullname</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fas fa-user"></i></span>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            id="name" placeholder="Enter full name" value="{{ old('name') }}">
                     </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                    id="email" placeholder="Enter email" value="{{ old('email') }}">
-                @error('email')
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    @error('name')
+                        <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                            <i class="fas fa-exclamation-circle"></i> {{ $message }}
+                            <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-3"> <!-- Center-align the label -->
+                    <label for="email" class="form-label">Email address</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fas fa-envelope"></i></span>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" placeholder="Enter email" value="{{ old('email') }}">
                     </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                    id="password" placeholder="Enter password">
-                @error('password')
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    @error('email')
+                        <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                            <i class="fas fa-exclamation-circle"></i> {{ $message }}
+                            <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-3"> <!-- Center-align the label -->
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fas fa-lock"></i></span>
+                        <input type="password" name="password"
+                            class="form-control @error('password') is-invalid @enderror" id="password"
+                            placeholder="Enter password">
                     </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="cpassword" class="form-label">Confirm Password</label>
-                <input type="password" name="password_confirmation" class="form-control" id="cpassword"
-                    placeholder="Confirm password">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <div class="mt-3">
+                    @error('password')
+                        <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                            <i class="fas fa-exclamation-circle"></i> {{ $message }}
+                            <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-3"> <!-- Center-align the label -->
+                    <label for="cpassword" class="form-label">Confirm Password</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fas fa-lock"></i></span>
+                        <input type="password" name="password_confirmation" class="form-control" id="cpassword"
+                            placeholder="Confirm password">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Register</button>
+            </form>
+            <div class="mt-3 text-center">
                 Already have an account? <a href="{{ route('user.login') }}">Login here</a>.
             </div>
-        </form>
+        </div>
     </div>
 
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous">
-    </script>
+    <!-- Add Bootstrap and Popper.js scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 
